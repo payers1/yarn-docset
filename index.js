290f5014cdba47bc05d9b564470fee625f16963f
Phill Ayers <phillip.ayers@sharpnotions.com>
2017-05-20 14:51:21 -0400

    Update script to convert online path to local path

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ const location = __dirname + '/yarn.docset/Contents/Resources/docSet.dsidx';
 const createTable = 'CREATE TABLE searchIndex(id INTEGER PRIMARY KEY, name TEXT, type TEXT, path TEXT)';
 const createIndex = 'CREATE UNIQUE INDEX anchor ON searchIndex (name, type, path)';
 
-const addRow = (name, path) => `
-  INSERT OR IGNORE INTO searchIndex(name, type, path)
-  VALUES (${JSON.stringify(name)}, 'Command', ${JSON.stringify(path)})
-  `
+const addRow = (name, onlinePath) => {
+  const onlinePathParts = onlinePath
+                          .replace('https://yarnpkg.com/en/docs', '.')
+                          .split('#');
+const localPath = [onlinePathParts[0], '/index.html#', onlinePathParts[1]].join('');
+  return `
+    INSERT OR IGNORE INTO searchIndex(name, type, path)
+    VALUES (${JSON.stringify(name)}, 'Command', ${JSON.stringify(localPath)})
+    `
+}
 
 return P.resolve(db.open(location))
   .then(() => db.all('DROP TABLE IF EXISTS searchIndex'))
